927365b5985e2cb664d612c3695977abeba650ca
Juan Fumero <juan.fumero@manchester.ac.uk>
2023-10-06 09:24:04 +0200

    run script added

--- readWriteBenchmark/run.sh
+++ readWriteBenchmark/run.sh
@@ -1,4 +1,14 @@
 
 
-## 8129 * 8192 float elements : 256MB
+#        256MB     512MB    1024MB
+sizes=( 67108864 134217728 268435456 )
+offsets=( 0 16 20 24 128 )
 
+for offset in "${offsets[@]}"
+do
+    for size in "${sizes[@]}"
+    do
+        echo "./read-write-benchmark -s $size -v $offset"
+        ./read-write-benchmark -s $size -v $offset >> result_$offset.txt
+    done
+done
